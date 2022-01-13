6db64455ab1f9ae4c20933c82ad38096843baacb
Gus-ta-vo <gustavo.gaudiano@naranjax.com>
2022-01-12 21:10:53 -0300

    First Comit from local

---
+++ 00-primeros-pasos/funciones-flecha.js
@@ -0,0 +1,9 @@
+var sumaNumeros = (a, b) => a + b
+var restaNumeros = (a, b) => a - b
+var multiplicaNumeros = (a, b) => a * b
+var divideNumeros = (a, b) => a / b
+
+console.log(sumaNumeros(5,3))
+console.log(restaNumeros(5,3))
+console.log(multiplicaNumeros(5,3))
+console.log(divideNumeros(5,3))
