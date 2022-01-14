3414082d02654abfadb75dd8ccd962b3a8a07c11
Gus-ta-vo <gustavo.gaudiano@naranjax.com>
2022-01-14 20:03:21 -0300

    Cambios para grabar el archivo con el resultado

--- Modulo02/app.js
+++ Modulo02/app.js
@@ -1,7 +1,20 @@
-console.log("Modulo 2 tarea 1");
+var fs = require('fs');
 
+console.log("Modulo 2 tarea 2");
+
+let resultado = ""
 const cinco = 5
 for (let numero = 1; numero < 11; numero++){
 //    console.log(cinco + " X " + numero + " = " + cinco*numero)
-    console.log(`${cinco} X ${numero} = ${cinco * numero}`);
+    resultado= resultado + (`${cinco} X ${numero} = ${cinco * numero}`);
+    if (numero < 10) {
+        resultado= resultado + (`\n`);
+    } 
 }
+console.log(resultado);
+
+fs.writeFile("tabla-5.txt", resultado, (errorArchivo) => {
+    if (errorArchivo) throw errorArchivo;
+    console.log('Archivo Grabado!');
+});
+
