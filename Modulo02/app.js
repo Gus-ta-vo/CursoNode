b2eef4aa399896364d3c0a3bad2025b9592ff4db
Gus-ta-vo <gustavo.gaudiano@naranjax.com>
2022-01-14 19:10:35 -0300

    Modulo 2 tarea 1

---
+++ Modulo02/app.js
@@ -0,0 +1,7 @@
+console.log("Modulo 2 tarea 1");
+
+const cinco = 5
+for (let numero = 1; numero < 11; numero++){
+//    console.log(cinco + " X " + numero + " = " + cinco*numero)
+    console.log(`${cinco} X ${numero} = ${cinco * numero}`);
+}
