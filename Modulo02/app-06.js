8c8e133454e19354524b2a9441850b888da0bd6d
Gus-ta-vo <gustavo.gaudiano@naranjax.com>
2022-01-19 19:07:38 -0300

    Muestra .argv de yargs

---
+++ Modulo02/app-06.js
@@ -0,0 +1,34 @@
+//const argumentos = require("process") No fue necesario agregar, lo toma sin require
+const colors = require('colors')
+const crearArchivo = require ("./creararchivo.js");
+const parametros = require ("yargs").argv
+
+console.log(parametros)
+//const parametros = process.argv
+
+/*
+console.log(colors.rainbow("Generador tabla de multiplicar"))
+let argumento = parametros[2].split("=")
+const base = Number(argumento[1])
+if ( argumento[0] == "--base" && (base > 0) ){
+        console.log("Parametro correcto. Procesando...")
+        let resultado = ""
+        for (let numero = 1; numero < 11; numero++){
+        //    console.log(cinco + " X " + numero + " = " + cinco*numero)
+            resultado= resultado + (`${base} X ${numero} = ${base * numero}`);
+            if (numero < 10) {
+                resultado= resultado + (`\n`);
+            } 
+        }
+        console.log(resultado);
+        
+        const nombreArchivo = "tabla-" + base + ".txt"
+        crearArchivo.crearArchivo(nombreArchivo, resultado)
+    }
+    else{
+    console.log("Parametro no reconocido. Intente con --base=<numero>")
+}
+
+// Con la siguiente linea grabo el archivo con el contenido Hola, sin tener en cuenta si fue correcto o incorrecto
+// fs.writeFile("pepe","Hola",()=>{})
+*/
