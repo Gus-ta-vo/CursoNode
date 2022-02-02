b00d482d1723c2a96175255381147e4ef3a080ca
Gus-ta-vo <gustavo.gaudiano@naranjax.com>
2022-02-02 20:07:58 -0300

    M03T04 - Reemplazo de menu por inquirer

--- app-lista-tareas/app.js
+++ app-lista-tareas/app.js
@@ -1,13 +1,13 @@
-const mensajes = require('./helpers/mensajes');
+const mensajes = require('./helpers/inquirer');
 
 console.clear();
 
 const main = async () => {
-  let opcion = '0';
+  let opcion = 0;
   do {
     opcion = await mensajes.mostrarMenu();
     await mensajes.pausa();
-  } while (opcion !== '0');
+  } while (opcion.opcion !== 0);
 };
 
 main();
---
+++ app-lista-tareas/helpers/inquirer.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+
+const mostrarMenu = async () => {
+  console.clear();
+  console.log(colors.green('======================'));
+  console.log('Seleccione una opción');
+  console.log(colors.green('======================'));
+  const opcion = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'opcion',
+        message: 'Ingrese una opción',
+        choices: [
+          { value: 1, name: '1 . Crear tarea' },
+          { value: 2, name: '2 . Listar tareas' },
+          { value: 3, name: '3 . Listar tareas completadas' },
+          { value: 4, name: '4 . Listar tareas pendientes' },
+          { value: 5, name: '5 . Completar tareas(s)' },
+          { value: 6, name: '6 . Borrar tarea' },
+          { value: 0, name: '0 . Salir' },
+        ],
+      },
+    ]);
+  return opcion;
+};
+
+const pausa = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'pausa',
+        message: `Presione ${colors.green('ENTER')} para continuar`,
+      },
+    ]);
+};
+
+module.exports = { mostrarMenu, pausa };
